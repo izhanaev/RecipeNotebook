61c54213c59b1714a729e7ad7bf5e8911f9ef0a3
Ilyas Zhanaev <izhanaev@mail.ru>
2025-01-11 15:22:01 +0300

    Initial Commit

---
+++ RecipeNotebook/RecipeNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeNotebookApp.swift
+//  RecipeNotebook
+//
+//  Created by Ильяс Жанаев on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
